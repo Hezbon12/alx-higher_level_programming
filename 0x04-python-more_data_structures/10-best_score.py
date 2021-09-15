e1960906b188eeca08fc4fa86439f79a0ca65cfc
Hezbon12 <otienohezbon12@gmail.com>
2021-09-15 23:03:09 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    new_dict = {}
-    for k, v in my_dict.items():
-        new_dict[k] = v * 2
-    return new_dict
+def best_score(my_dict):
+    if my_dict is None or my_dict == {}:
+        return None
+    biggest = max(my_dict.values())
+    for key, value in my_dict.items():
+        if value is biggest:
+            return key
