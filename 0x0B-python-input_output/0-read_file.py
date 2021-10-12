fdf34222f7e68cabbb0c7f05c53131f134541fe4
Hezbon12 <otienohezbon12@gmail.com>
2021-10-12 23:47:06 +0300

    input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Module 0-read_file
+Contains function that reads and prints contents from file
+"""
+
+
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as fd:
-        for line in fd:
-            print(line, end="")
+    """Read and print text from file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
