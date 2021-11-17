73f9ecc12781a1fca87502b4b1217f78912bf0bf
Hezbon12 <otienohezbon12@gmail.com>
2021-11-17 23:55:52 +0300

    more sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -7,4 +7,4 @@ SELECT t.title, g.name
 FROM tv_shows AS t
 LEFT JOIN tv_show_genres AS m ON t.id = m.show_id
 LEFT JOIN tv_genres AS g ON m.genre_id = g.id
-ORDER BY t.title ASC;
+ORDER BY t.title, g.name;
