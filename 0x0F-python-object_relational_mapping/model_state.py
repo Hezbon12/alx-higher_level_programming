619b52b85d083630c324243613fa1ffd532c668b
Hezbon12 <otienohezbon12@gmail.com>
2021-12-22 16:21:02 +0300

    mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,10 +10,10 @@ Base = declarative_base()
 
 
 class State(Base):
-        """
-            Class State; instance of Base
-                Linked to MySQL table "states"
-                    """
-                        __tablename__ = "states"
-                            id = Column(Integer, nullable=False, primary_key=True)
-                                name = Column(String(128), nullable=False)
+"""
+Class State; instance of Base
+Linked to MySQL table "states"
+"""
+__tablename__ = "states"
+id = Column(Integer, nullable=False, primary_key=True)
+name = Column(String(128), nullable=False)
