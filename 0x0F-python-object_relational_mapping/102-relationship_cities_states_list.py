5eb77ab7ec1cbe5c85aa092320f19aada85adf68
Hezbon12 <otienohezbon12@gmail.com>
2021-12-24 18:46:52 +0300

    mapping

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -2,7 +2,6 @@
 """
 use table relationship to access and print city and state
 parameters given to script: username, password, database
-
 """
 
 from sys import argv
@@ -19,16 +18,14 @@ if __name__ == "__main__":
     passwd = argv[2]
     db = argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                                                           format(user, passwd, db), pool_pre_ping=True)
+                            format(user, passwd, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # use table relationship to access and print city and state
-    rows = session.query(State).order_by(State.id).all()
-    for state in rows:
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+    rows = session.query(City).order_by(City.id).all()
+    for city in rows:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
 
     session.close()
