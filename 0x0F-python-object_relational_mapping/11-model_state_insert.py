354af414b9922d20239d5f6cebe803ff0ec622bb
Hezbon12 <otienohezbon12@gmail.com>
2021-12-24 14:41:38 +0300

    mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -2,6 +2,7 @@
 """
 update state: given id, change state name
 parameters given to script: username, password, database
+
 """
 
 from sys import argv
@@ -12,21 +13,21 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
 
-        # make engine for database
-        mysql username = argv[1]
-        mysql password = argv[2]
-        database name = argv[3]
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                                format(mysql username, mysql password, database name), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    # make engine for database
+    mysql username = argv[1]
+    mysql password = argv[2]
+    database name = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                            format(mysql username, mysql password, database name), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        # add new state and commit to table
-        new = State(name="Louisiana")
-        session.add(new)
-        session.commit()
+    # add new state and commit to table
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
 
-        print("{:d}".format(new.id))
+    print("{:d}".format(new.id))
 
-        session.close()
+    session.close()
